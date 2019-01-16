d566262a3bc61d8ebc9a547aa4ae9a3c200fe788
Andy Ng <andy.hy.ng@gmail.com>
2019-01-16 14:10:52 -0500

    added app constructor, handleChange and initial form

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,35 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      searchRequest: "potato+chips",
+      postalCode: "",
+      chipsList: [],
+    }
+  }
+
+  handleChange = (e: any) => {
+    this.setState({
+      searchRequest: e.target.value
+    })
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
+        <h1>Couch Potato</h1>
+        <h2>{`For the couch potatoes looking for cheap potato (chips)`}</h2>
+        <form action="" onChange={this.handleChange}>
+          <select name="chipBrand" id="brand">
+            <option value="potato+chips" defaultValue="true">All</option>
+            <option value="ruffles">Ruffles</option>
+            <option value="miss+vickie">Miss Vickie's</option>
+            <option value="lays">Lay's</option>
+          </select>
+        </form>
+        {/* <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.tsx</code> and save to reload.
@@ -19,7 +44,7 @@ class App extends Component {
           >
             Learn React
           </a>
-        </header>
+        </header> */}
       </div>
     );
   }
